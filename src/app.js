e137eb58d1e5c85c10188c8214a18316ad440106
Miguel Ojeda <113554869+Alejandro959521@users.noreply.github.com>
2023-06-01 20:25:14 +0000

    Ejecicio completado

--- src/app.js
+++ src/app.js
@@ -7,5 +7,27 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  document.querySelector("#excuse").innerHTML =
+    who[getRandomInt(who.length)] +
+    " " +
+    what[getRandomInt(what.length)] +
+    " " +
+    action[getRandomInt(action.length)] +
+    " " +
+    when[getRandomInt(when.length)];
 };
